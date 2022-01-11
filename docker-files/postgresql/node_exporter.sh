1cf56069566ef68dbe5cfc8b9cea2153b7756fbb
root <mix-swir@yandex.ru>
2022-01-11 09:21:26 +0300

    report example

---
+++ docker-files/postgresql/node_exporter.sh
@@ -0,0 +1,4 @@
+wget https://github.com/prometheus/node_exporter/releases/download/v*/node_exporter-*.*-amd64.tar.gz
+tar xvfz node_exporter-*.*-amd64.tar.gz
+cd node_exporter-*.*-amd64
+./node_exporter
